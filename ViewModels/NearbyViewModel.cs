3fe7194676970b8e3ee979ddfeb4a78c51555fd4
2256450597 <111105628+2256450597@users.noreply.github.com>
2026-05-29 14:09:23 +0800

    Replace WebView+Leaflet with static OpenStreetMap image (more reliable on Android)

--- ViewModels/NearbyViewModel.cs
+++ ViewModels/NearbyViewModel.cs
@@ -46,11 +46,11 @@ public class NearbyViewModel : BaseViewModel
         set => SetProperty(ref _hasLocation, value);
     }
 
-    private HtmlWebViewSource? _mapSource;
-    public HtmlWebViewSource? MapSource
+    private string _mapUrl = string.Empty;
+    public string MapUrl
     {
-        get => _mapSource;
-        set => SetProperty(ref _mapSource, value);
+        get => _mapUrl;
+        set => SetProperty(ref _mapUrl, value);
     }
 
     private bool _hasMap;
@@ -120,8 +120,12 @@ public class NearbyViewModel : BaseViewModel
 
                 LocationInfo = $"Found {places.Count} stores nearby.";
 
-                // Generate OpenStreetMap via Leaflet
-                GenerateMap(location.Latitude, location.Longitude, places);
+                // Static map from OpenStreetMap with a marker for user position
+                MapUrl = $"https://staticmap.openstreetmap.de/staticmap.php" +
+                         $"?center={location.Latitude},{location.Longitude}" +
+                         $"&zoom=15&size=600x300&maptype=mapnik" +
+                         $"&markers={location.Latitude},{location.Longitude},red-pushpin";
+                HasMap = true;
             }
             else
             {
@@ -136,41 +140,5 @@ public class NearbyViewModel : BaseViewModel
         {
             IsLocating = false;
         }
-    }
-
-    private void GenerateMap(double lat, double lon, List<NearbyPlace> places)
-    {
-        var markersJs = $"var you = L.marker([{lat}, {lon}]).addTo(map).bindPopup('You are here').openPopup();\n";
-        foreach (var place in places)
-        {
-            markersJs += $"L.marker([{place.Latitude}, {place.Longitude}]).addTo(map).bindPopup('<b>{place.Name}</b><br/>{place.Address}<br/>{place.DistanceKm:F1} km');\n";
-        }
-
-        var html = $@"<!DOCTYPE html>
-<html>
-<head>
-<meta charset='utf-8'/>
-<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
-<link rel='stylesheet' href='https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'/>
-<script src='https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'></script>
-<style>
-  html,body,#map{{margin:0;padding:0;height:100%;width:100%}}
-</style>
-</head>
-<body>
-<div id='map'></div>
-<script>
-  var map = L.map('map').setView([{lat}, {lon}], 16);
-  L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
-    attribution: '&copy; OpenStreetMap contributors',
-    maxZoom: 19
-  }}).addTo(map);
-  {markersJs}
-</script>
-</body>
-</html>";
-
-        MapSource = new HtmlWebViewSource { Html = html };
-        HasMap = true;
     }
 }
